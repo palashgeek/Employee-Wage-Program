339282a7604147b8e1934d8d146d7bfac06925a4
palashgeek <palashgupta7742@gmail.com>
2024-02-22 18:41:55 +0530

    [Palash] Refactor: Python script using Match case

--- employee-wage.py
+++ employee-wage.py
@@ -6,16 +6,28 @@ PART_TIME_WAGE_HR = 4
 
 def emp_attendence():
     attendence = random.randint(0,2)
-    if attendence == 1:
-        return 1
-    elif attendence ==2:
-        return 2
-    else:
-        return 0
+    match attendence:
+        case 0:
+            return 0
+        case 1:
+            return 1
+        case 2:
+            return 2
     
 def daily_employee_wage():
 
-    if emp_attendence() == 1:
+    match emp_attendence():
+        case 0:
+            return "The Employee is Absent and the daily wage is 0."
+        
+        case 1:
+            daily_wage = FULL_TIME_WAGE_PER_HR * FULL_DAY_HR
+            return "Employee is present for full time and the daily wage of the employee is {}".format(daily_wage)
+        case 2:
+             daily_wage = FULL_TIME_WAGE_PER_HR * PART_TIME_WAGE_HR 
+             return "Employee is present only for part time and the daily wage of employee is {}".format(daily_wage)
+
+''' if emp_attendence() == 1:
         daily_wage = FULL_TIME_WAGE_PER_HR * FULL_DAY_HR
         return "Employee is present for full time and the daily wage of the employee is {}".format(daily_wage)
     elif emp_attendence() == 2:
@@ -23,7 +35,7 @@ def daily_employee_wage():
         return "Employee is present only for part time and the daily wage of employee is {}".format(daily_wage)
     else:
         return "The Employee is Absent and the daily wage is 0."
-    
+    '''
 
 
 if __name__ == "__main__":
