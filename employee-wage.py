5a8f1da9c3378350cf596d1f3bce5309bbe2b30d
palashgeek <palashgupta7742@gmail.com>
2024-02-22 15:12:58 +0530

    [Farzana]ADD: Script to calculate the daily wage of an employee

--- employee-wage.py
+++ employee-wage.py
@@ -5,7 +5,16 @@ def emp_attendence():
         print("Employee is present")
     else:
         print("Employee is not present")
+def daily_employee_wage():
+    full_time_wage_per_hr = 20
+    full_day_hr = 8
+
+    if emp_attendence == 1:
+        daily_wage = full_time_wage_per_hr * full_day_hr
+        return "the daily wage for an full time employee is: {}".format(daily_wage)
+    else:
+        return "the employee is not present"
 
 if __name__ == "__main__":
-    emp_attendence()
+    print(daily_employee_wage())
 
