4bdcf49111c1ba5b31f79c5159d16d7ad788ae11
palashgeek <palashgupta7742@gmail.com>
2024-02-23 12:54:00 +0530

    [Palash] Add: cal of working hours and days

--- employee-wage.py
+++ employee-wage.py
@@ -3,38 +3,44 @@ import random
 FULL_TIME_WAGE_PER_HR = 20
 FULL_DAY_HR = 8
 PART_TIME_WAGE_HR = 4
+MONTHLY_WORKING_DAYS = 20
+MONTHLY_WORKING_HRS = 100
 
 def emp_attendence():
-    attendence = random.randint(0,2)
-    match attendence:
-        case 0:
-            return 0
-        case 1:
-            return 1
-        case 2:
-            return 2
+    return random.randint(0,2)
     
-def daily_employee_wage():
+    
+def daily_employee_wage(attendance):
 
-    match emp_attendence():
-        case 0:
+    if attendance == 0:
             return 0
         
-        case 1:
-            daily_wage = FULL_TIME_WAGE_PER_HR * FULL_DAY_HR
-            return daily_wage
+    elif attendance ==1:
+            return FULL_TIME_WAGE_PER_HR * FULL_DAY_HR 
         
-        case 2:
-             daily_wage = FULL_TIME_WAGE_PER_HR * PART_TIME_WAGE_HR 
-             return daily_wage
+    elif attendance == 2:
+             return FULL_TIME_WAGE_PER_HR * PART_TIME_WAGE_HR 
 
 def month_wage():
     montly_wage = 0
-    for i in range(20):
-       montly_wage += daily_employee_wage()
+    total_hrs = 0
+    total_days = 0
+
+    while total_hrs < MONTHLY_WORKING_HRS and total_days < MONTHLY_WORKING_DAYS:
+        attendance = emp_attendence()
+        daily_wage = daily_employee_wage(attendance)
+        montly_wage += daily_wage
+
+        if attendance != 0:
+             total_days +=1
+             if attendance == 1:
+                  total_hrs += FULL_DAY_HR
+             elif attendance == 2:
+                  total_hrs += PART_TIME_WAGE_HR
+    
     return montly_wage
         
-        
+
 
 if __name__ == "__main__":
     print(month_wage())
